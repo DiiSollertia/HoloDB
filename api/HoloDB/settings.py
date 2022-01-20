c47d9a9534199f76fe70c5a69b428c2e5063b8ac
DiiSollertia <xz.khoo.work@gmail.com>
2022-01-20 22:18:14 +0800

    added missing os module

--- api/HoloDB/settings.py
+++ api/HoloDB/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # Module for environment variables
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
